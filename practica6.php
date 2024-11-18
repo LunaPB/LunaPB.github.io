318765a5e8bbf3f7412cdab5d70620f7178bb869
Brian Fernando Luna Padrón <l21530309@cancun.tecnm.mx>
2024-11-18 13:58:39 -0500

    Update practica6.php

--- practica6.php
+++ practica6.php
@@ -1,14 +1,18 @@
 <?php
 
 echo "<h1> Práctica: Conexión a mi servidor de datos MYSQL </h1><br><br>";
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:lunapb.database.windows.net,1433; Database = programacionweb", "CloudSAdcf09f47", "ProgWeb78*Test");
+    $conn = new PDO("sqlsrv:server = tcp:lunapb.database.windows.net,1433; Database = programacionweb", "CloudSAdcf09f47", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-echo "Tu conexión ha sido exitosa";
-}
 catch (PDOException $e) {
-    print("La conexión ha fallado.");
+    print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "CloudSAdcf09f47", "pwd" => "{your_password_here}", "Database" => "programacionweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:lunapb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
