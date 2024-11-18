62b63d8215f3688532cb00be269173ec2ab41607
Brian Fernando Luna Padrón <l21530309@cancun.tecnm.mx>
2024-11-18 14:22:40 -0500

    Update practica6.php

--- practica6.php
+++ practica6.php
@@ -5,8 +5,9 @@ echo "<h1> Práctica: Conexión a mi servidor de datos MYSQL </h1><br><br>";
 try {
     $conn = new PDO("sqlsrv:server = tcp:lunapb.database.windows.net,1433; Database = programacionweb", "CloudSAdcf09f47", "ProgWeb78*Test");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    print "La conexión con el servidor de datos SQL ha sido exitosa (En caso de un error en la conexión aparecera un mensaje adicional a este)";
 }
-print "La conexión con el servidor de datos SQL ha sido exitosa (En caso de un error en la conexión aparecera un mensaje adicional a este)";
+
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
