ba975ec2c81c7c68a62b7523c3649b871b9c4b51
Brian Fernando Luna Padrón <l21530309@cancun.tecnm.mx>
2024-11-21 14:27:21 -0500

    Update practica9.php

--- practica9.php
+++ practica9.php
@@ -2,31 +2,30 @@
 
 echo "<h1> Práctica: Conexión a mi servidor de datos MYSQL </h1><br><br>";
 
-// PHP Data Objects(PDO) Sample Code:
 try {
+    // Establecer conexión
     $conn = new PDO("sqlsrv:server = tcp:lunapb.database.windows.net,1433; Database = programacionweb", "CloudSAdcf09f47", "ProgWeb78*Test");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    print "La conexión con el servidor de datos SQL ha sido exitosa";
+    echo "La conexión con el servidor de datos SQL ha sido exitosa.<br>";
 
-        $createTableQuery = "CREATE TABLE dbo.$tablaprueba (";
-        foreach ($columns as $column) {
-            $createTableQuery .= "$column[ID] $column[int], ";
-        }
-        $createTableQuery = rtrim($createTableQuery, ", ") . ")";
+    // Definir datos para la tabla
+    $tablaprueba = "tabla_prueba";
+    $columns = [
+        ["ID" => "id", "type" => "INT PRIMARY KEY"],
+        ["ID" => "nombre", "type" => "VARCHAR(50)"]
+    ];
 
-        $conn->exec($createTableQuery);
-        echo "La tabla $tableName se ha creado con exito";
+    // Crear tabla
+    $createTableQuery = "CREATE TABLE dbo.$tablaprueba (";
+    foreach ($columns as $column) {
+        $createTableQuery .= "{$column['ID']} {$column['type']}, ";
+    }
+    $createTableQuery = rtrim($createTableQuery, ", ") . ")";
+    
+    $conn->exec($createTableQuery);
+    echo "La tabla $tablaprueba se ha creado con éxito.";
 }
-
 catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+    die("Error al conectar con SQL Server: " . $e->getMessage());
 }
-
-
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSAdcf09f47", "pwd" => "ProgWeb78*Test", "Database" => "programacionweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:lunapb.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
